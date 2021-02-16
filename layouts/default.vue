149f96ab17458ddafc2941d6597e3ada3cf8fb72
olivervorasai <olivervorasai@gmail.com>
2021-02-16 12:53:55 -0800

    Initial Commit: Npm JS Vuetify Content SPA Static

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <v-app-bar absolute flat app> </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+      ],
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-row justify="center" align="center">
+    <v-col cols="12" sm="8" md="6"> </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      script: [
+        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
+      ],
+    }
+  },
+}
+</script>
