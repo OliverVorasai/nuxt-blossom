7b9d324fb8de89cca960480134bb252c7bd6d3e6
olivervorasai <olivervorasai@gmail.com>
2021-02-17 19:34:53 -0800

    Explanation collection: body now optional

---
+++ components/Explanation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>{{ explanation.title }}</h2>
+    <nuxt-content :document="explanation"></nuxt-content>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    explanations: {
+      type: Array,
+      required: true,
+      default: () => [],
+    },
+    explanationId: {
+      type: String,
+      required: true,
+      default: () => '',
+    },
+  },
+  computed: {
+    explanation() {
+      return this.explanations.find((obj) => {
+        return obj.id === this.explanationId
+      })
+    },
+  },
+}
+</script>
