c6f92a0d731ac9ef314601f3ca6b0731ed8d77c9
olivervorasai <olivervorasai@gmail.com>
2021-02-18 22:06:19 -0800

    Add slot component to color entire background of other components

---
+++ components/ColorSlot.vue
@@ -0,0 +1,29 @@
+<!-- Use slot to overcome margins -->
+<template>
+  <div class="color-slot-container" :style="styles">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      required: true,
+      default: () => '#000000',
+    },
+  },
+  computed: {
+    styles() {
+      return { '--color': this.color }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.color-slot-container {
+  background-color: var(--color);
+}
+</style>
