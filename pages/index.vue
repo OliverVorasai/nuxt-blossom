e73b2f3f135638c9760f731f128b58456bd20bb0
olivervorasai <olivervorasai@gmail.com>
2021-02-19 00:43:30 -0800

    Add general layout of index

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,181 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6"> </v-col>
-  </v-row>
+  <div>
+    <!-- Hero Section -->
+    <v-container>
+      <v-row justify="center" align="center" class="mx-6 mt-12 mt-md-0">
+        <v-col cols="12" xl="10">
+          <v-row justify="start" align="center">
+            <v-col cols="12" md="6">
+              <div class="hero-content">
+                <h1 class="text-h3 text-sm-h2 text-xl-h1">
+                  Web Design for Non-Profits and Small Businesses
+                </h1>
+                <v-divider class="mt-12"></v-divider>
+              </div>
+            </v-col>
+            <v-col cols="12" md="6">
+              <v-img :src="require('~/assets/nature.svg')"></v-img>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- First Blossom Explanation -->
+    <angled-border color="#f3fafe" invert-x></angled-border>
+    <color-slot color="#f3fafe">
+      <v-container>
+        <v-row justify="center" align="center" class="mx-6">
+          <v-col cols="12" sm="8" md="6" class="text-center">
+            <explanation
+              :explanations="explanations"
+              explanation-id="explanation-what-is-blossom"
+            ></explanation>
+          </v-col>
+        </v-row>
+      </v-container>
+    </color-slot>
+    <angled-border color="#f3fafe" invert-y></angled-border>
+    <!-- Triple Aspect Explanation -->
+    <v-container>
+      <v-row justify="center" align="center" class="mx-6 mb-12">
+        <v-col cols="12" sm="8">
+          <v-row justify="center">
+            <v-col cols="12" sm="7" md="4" class="text-center">
+              <explanation
+                :explanations="explanations"
+                explanation-id="explanation-fast-response"
+                image-above
+              ></explanation>
+            </v-col>
+            <v-col cols="12" sm="7" md="4" class="text-center">
+              <explanation
+                :explanations="explanations"
+                explanation-id="explanation-clean-aesthetics"
+                image-above
+              ></explanation>
+            </v-col>
+            <v-col cols="12" sm="7" md="4" class="text-center">
+              <explanation
+                :explanations="explanations"
+                explanation-id="explanation-responsive-design"
+                image-above
+              ></explanation>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- Price Explanation -->
+    <v-container>
+      <v-row justify="start" align="center" class="mx-6 mb-12">
+        <v-col cols="12" md="6">
+          <v-img
+            :src="require('~/assets/ticket.svg')"
+            max-height="500"
+            contain
+          ></v-img>
+        </v-col>
+        <v-col cols="12" md="6">
+          <h2>$0 Per Month</h2>
+          <h3>For Non-Profit Organizations</h3>
+          <h2>$50 Starting Per Month</h2>
+          <h3>For Small businesses</h3>
+          <p>
+            Prices reflect the need and capabilities of all parties involved and
+            include up to 3 website pages as a starting package. More expansive
+            projects may include a custom pricing package.
+          </p>
+          <p>The website address and all content belong to you.</p>
+          <p>No security deposit, no cancellation fees.</p>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- Portfolio Section -->
+    <v-container>
+      <v-row justify="center" align="center" class="mb-12">
+        <v-col cols="12" xl="10">
+          <explanation
+            :explanations="explanations"
+            explanation-id="explanation-portfolio"
+          ></explanation>
+          <v-row justify="center" align="center" class="mx-sm-6 mx-md-0">
+            <v-col cols="12" md="4">
+              <portfolio-card
+                :portfolios="portfolios"
+                portfolio-id="portfolio-nam-quang-temple"
+              ></portfolio-card>
+            </v-col>
+            <v-col cols="12" md="4">
+              <portfolio-card
+                :portfolios="portfolios"
+                portfolio-id="portfolio-zien-hong"
+              ></portfolio-card>
+            </v-col>
+            <v-col cols="12" md="4">
+              <portfolio-card
+                :portfolios="portfolios"
+                portfolio-id="portfolio-scrapmeals"
+              ></portfolio-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+    <!-- Second Blossom Explanation -->
+    <angled-border color="#f3fafe" invert-x invert-y></angled-border>
+    <color-slot color="#f3fafe">
+      <v-container>
+        <v-row justify="center" align="center">
+          <v-col cols="12" sm="8" md="6" class="text-center">
+            <explanation
+              :explanations="explanations"
+              explanation-id="explanation-blossom-web-design"
+            ></explanation>
+          </v-col>
+        </v-row>
+      </v-container>
+    </color-slot>
+    <angled-border color="#f3fafe"></angled-border>
+    <!-- Price Package Section -->
+    <v-container>
+      <v-row justify="center" align="center">
+        <v-col cols="10" sm="8" md="6" lg="4">
+          <price-card
+            :packages="packages"
+            package-id="package-non-profit"
+          ></price-card>
+        </v-col>
+        <v-col cols="10" sm="8" md="6" lg="4">
+          <price-card
+            :packages="packages"
+            package-id="package-small-business"
+          ></price-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
 export default {
-  head() {
-    return {
-      script: [
-        { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' },
-      ],
-    }
+  async asyncData({ $content }) {
+    const explanations = await $content('explanations').fetch()
+    const portfolios = await $content('portfolios').fetch()
+    const packages = await $content('packages').fetch()
+    return { explanations, portfolios, packages }
   },
 }
 </script>
+
+<style scoped lang="scss">
+.hero-content {
+  h1 {
+    font-display: swap;
+    font-family: Newsreader !important;
+  }
+  .v-divider {
+    border-color: $primary;
+    border-width: 2px;
+  }
+}
+</style>
