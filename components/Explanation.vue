b7fca03faf14408b31b64fcec2f9217ef9786c73
olivervorasai <olivervorasai@gmail.com>
2021-02-19 20:41:01 -0800

    Add price explanation, add color options to explanation

--- components/Explanation.vue
+++ components/Explanation.vue
@@ -1,26 +1,26 @@
 <template>
-  <div>
+  <div :style="styles">
     <v-row
       v-if="explanation.image && imageAbove"
       justify="center"
       align="center"
-      class="my-6"
+      class="mt-6"
     >
       <img :src="explanation.image" width="85" />
     </v-row>
-    <v-row justify="center">
+    <v-row justify="center" class="mb-2">
       <h2
         class="explanation-title"
         :class="uppercaseTitle ? 'text-uppercase' : ''"
       >
-        {{ explanation.title }}
+        <span class="title-color">{{ title.first }} </span>{{ title.rest }}
       </h2>
     </v-row>
     <v-row
       v-if="explanation.image && !imageAbove"
       justify="center"
       align="center"
-      class="mt-4 mb-6"
+      class="mt-2 mb-6"
     >
       <span>-&nbsp;-&nbsp;-&nbsp;-&nbsp;-&nbsp;-&nbsp;</span>
       <img :src="explanation.image" width="35" />
@@ -53,12 +53,61 @@ export default {
       required: false,
       default: () => false,
     },
+    titleColor: {
+      type: String,
+      required: false,
+      default: () => '#000000',
+    },
+    customColorOne: {
+      type: String,
+      required: false,
+      default: () => '#000000',
+    },
+    customColorTwo: {
+      type: String,
+      required: false,
+      default: () => '#000000',
+    },
+    customColorThree: {
+      type: String,
+      required: false,
+      default: () => '#000000',
+    },
+    customColorFour: {
+      type: String,
+      required: false,
+      default: () => '#000000',
+    },
   },
   computed: {
     explanation() {
       return this.explanations.find((obj) => {
         return obj.id === this.explanationId
       })
+    },
+    styles() {
+      return {
+        '--title-color': this.titleColor,
+        '--custom-color-1': this.customColorOne,
+        '--custom-color-2': this.customColorTwo,
+        '--custom-color-3': this.customColorThree,
+        '--custom-color-4': this.customColorFour,
+      }
+    },
+    title() {
+      const firstWord = this.explanation.title.split(' ')[0]
+      let restOfWord = this.explanation.title.substr(
+        this.explanation.title.indexOf(' ') + 1
+      )
+
+      if (restOfWord === firstWord) {
+        restOfWord = ''
+      }
+
+      return {
+        first: firstWord,
+        rest: restOfWord,
+      }
     },
   },
 }
@@ -74,10 +123,25 @@ export default {
   @media #{map-get($display-breakpoints, 'md-and-up')} {
     font-size: 2.5rem;
   }
+  .title-color {
+    color: var(--title-color);
+  }
 }
 
 ::v-deep .nuxt-content {
   font-display: swap;
   font-weight: 300;
+  .custom-color-1 {
+    color: var(--custom-color-1);
+  }
+  .custom-color-2 {
+    color: var(--custom-color-2);
+  }
+  .custom-color-3 {
+    color: var(--custom-color-3);
+  }
+  .custom-color-4 {
+    color: var(--custom-color-4);
+  }
 }
 </style>
